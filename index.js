6cef1d748460ac429e423e0c8540749e78919edc
Israr Ahmed <israrahmedpk1999@gmail.com>
2024-02-04 10:30:15 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express")
+const dbConnection=require("./dbConnection/connection")
+
+const app=express();
+
+dbConnection();
+app.get("/test", async(req,res)=>{
+    res.send("Hello")
+})
+
+const port=process.env.PORT || 8080;
+
+app.listen(port, ()=>{
+    console.log(`Server is listening at ${port}`)
+})
